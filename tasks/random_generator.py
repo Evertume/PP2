eef20bc54b516e1f5a1464a5f119ad6412e2d330
Dias Kuspanov <117449684+Evertume@users.noreply.github.com>
2023-02-16 16:40:40 +0600

    rand

---
+++ tasks/random_generator.py
@@ -0,0 +1,11 @@
+import time
+
+def generate_random_number(seed):
+    current_time = time.time()
+    random_num = int(current_time / seed) % 10
+
+    if random_num == 0:
+        return 1
+    else:
+        return random_num
+print(generate_random_number(1))
