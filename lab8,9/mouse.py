814ac6cf4aec70cea5f41f6c04603ceee5691e73
Dias Kuspanov <117449684+Evertume@users.noreply.github.com>
2023-03-31 13:55:10 +0600

    new paint

--- lab8,9/mouse.py
+++ lab8,9/mouse.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 
 def main():
     pygame.init()
@@ -10,6 +10,9 @@ def main():
     y = 0
     mode = 'blue'
     points = []
+    mouse_down = False
+    shape_mode = None
+
     
     while True:
         
@@ -38,46 +41,79 @@ def main():
                     mode = 'green'
                 elif event.key == pygame.K_b:
                     mode = 'blue'
+                elif event.key == pygame.K_c:
+                    shape_mode = 'circle'
+                elif event.key == pygame.K_q:
+                    shape_mode = 'rect'
+                elif event.key == pygame.K_e:
+                    mode = 'eraser'
+                elif event.key == pygame.K_l:
+                    shape_mode = None
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1: # left click grows radius
                     radius = min(200, radius + 1)
                 elif event.button == 3: # right click shrinks radius
                     radius = max(1, radius - 1)
-            
+                elif event.button == 2:
+                    if mode != 'eraser':
+                        mode = 'eraser'
+                    else:
+                        mode = 'blue'
+                mouse_down = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_down = False
+
             if event.type == pygame.MOUSEMOTION:
                 # if mouse moved, add point to list
-                position = event.pos
-                points = points + [position]
-                points = points[-256:]
+                if mouse_down:
+                    position = event.pos
+                    points = points + [position]
+                    points = points[-256:]
                 
         screen.fill((0, 0, 0))
         
         # draw all points
         i = 0
         while i < len(points) - 1:
-            drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
+            if shape_mode == 'circle':
+                drawCicleBetween(screen, i, points[i], points[i + 1], radius)
+            elif shape_mode == 'rect':
+                drawRectBetween(screen, i, points[i], points[i + 1], radius)
+            else:
+                drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
             i += 1
         
         pygame.display.flip()
         
         clock.tick(60)
+def drawCicleBetween(screen, index, start, end, width):
+    color = (255, 255, 255)
+    radius = int(math.sqrt((start[0] - end[0])**2 + (start[1] - end[1])**2))
+    pygame.draw.circle(screen, color, start,radius, width)
+    
+def drawRectBetween(screen, index, start, end, width):
+    color = (255, 255, 255)
+    rect = pygame.Rect(start, (end[0] - start[0], end[1] - start[1]))
+    pygame.draw.rect(screen, color, rect, width)
 
 def drawLineBetween(screen, index, start, end, width, color_mode):
     c1 = max(0, min(255, 2 * index - 256))
     c2 = max(0, min(255, 2 * index))
-    
+   
     if color_mode == 'blue':
         color = (c1, c1, c2)
     elif color_mode == 'red':
         color = (c2, c1, c1)
     elif color_mode == 'green':
         color = (c1, c2, c1)
-    
+    elif color_mode == 'eraser':
+        color = (0, 0, 0)
+        
     dx = start[0] - end[0]
     dy = start[1] - end[1]
     iterations = max(abs(dx), abs(dy))
-    
+        
     for i in range(iterations):
         progress = 1.0 * i / iterations
         aprogress = 1 - progress
